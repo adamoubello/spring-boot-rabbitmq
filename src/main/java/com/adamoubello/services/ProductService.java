f2ec143c649cdab712f1a14fa08a036dfc55d144
Bello the son of Sun <adamou.bello@gmail.com>
2024-08-18 22:54:08 +0200

    spring-boot-rabbitmq: initial commit of working repo

---
+++ src/main/java/com/adamoubello/services/ProductService.java
@@ -0,0 +1,21 @@
+package com.adamoubello.services;
+
+import com.adamoubello.commands.ProductForm;
+import com.adamoubello.domain.Product;
+
+import java.util.List;
+
+public interface ProductService {
+
+    List<Product> listAll();
+
+    Product getById(Long id);
+
+    Product saveOrUpdate(Product product);
+
+    void delete(Long id);
+
+    Product saveOrUpdateProductForm(ProductForm productForm);
+
+    void sendProductMessage(String id);
+}
